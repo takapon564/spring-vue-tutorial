21d7b59c2ff42c4d8eb3be2e358773eda0a97174
takapon564 <ku.takahito058@gmail.com>
2019-02-15 06:26:37 +0900

    first commit

---
+++ api/src/main/java/com/example/repo/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.example.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.model.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByAge(int age);
+}
